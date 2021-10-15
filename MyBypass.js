047aab72b8e3cd380bb51b3705e0c5d41aa9b7c4
Blackjack <78551687+Utku1114@users.noreply.github.com>
2021-10-15 21:02:46 +0300

    Update MyBypass.js

--- MyBypass.js
+++ MyBypass.js
@@ -3,7 +3,7 @@ var cloudscraper = require('cloudscraper');
 
 var options = {
   method: 'GET',
-  url:'https://famy.cc',
+  url:'https://dev.exitus.me/',
 };
  
 cloudscraper(options).then(console.log);
